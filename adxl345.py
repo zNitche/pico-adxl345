82112cfae3de6b635697fde29017d257241b551b
zNitche <53448802+zNitche@users.noreply.github.com>
2024-07-05 18:22:09 +0200

    sensor driver minor tweaks

--- adxl345.py
+++ adxl345.py
@@ -1,7 +1,7 @@
 import time
 import struct
-from micropython import const
 import machine
+from micropython import const
 
 
 DEVICE_ID_REGISTER_ADDR = const(0x00)
@@ -12,20 +12,22 @@ DATA_REGISTER_ADDR = const(0x32)
 ACTIVE_MODE = const(0x28)
 STANDBY_MODE = const(0x24)
 
+EARTH_GRAVITY = const(9.80665)
+SCALE_MULTIPLIER = const(0.004)
+
 
 class ADXL345:
     def __init__(self, i2c: machine.I2C, address=0x53):
         self.i2c = i2c
         self.address = address
 
-        self.__compensation_value = 0.039228
+        self.__final_readings_factor = EARTH_GRAVITY * SCALE_MULTIPLIER
 
     def is_device_accessible(self) -> bool:
         return True if self.address in self.i2c.scan() else False
 
     def check_connection(self) -> bool:
         data = self.i2c.readfrom_mem(self.address, DEVICE_ID_REGISTER_ADDR, 1)
-        time.sleep_ms(20)
 
         return True if data == bytearray((DEVICE_ID,)) else False
 
@@ -36,12 +38,9 @@ class ADXL345:
     def stop_measurements(self):
         self.i2c.writeto_mem(self.address, POWER_CONTROL_REGISTER_ADDR, bytearray([STANDBY_MODE]))
 
-    def get_xyz(self) -> tuple[float, float, float]:
+    def get_xyz(self) -> list[float]:
         data = self.i2c.readfrom_mem(self.address, DATA_REGISTER_ADDR, 6)
         raw_data_struct = struct.unpack("<hhh", data)
+        processed_data = [val * self.__final_readings_factor for val in raw_data_struct]
 
-        x_acceleration = raw_data_struct[0] * self.__compensation_value
-        y_acceleration = raw_data_struct[1] * self.__compensation_value
-        z_acceleration = raw_data_struct[2] * self.__compensation_value
-
-        return x_acceleration, y_acceleration, z_acceleration
+        return processed_data
